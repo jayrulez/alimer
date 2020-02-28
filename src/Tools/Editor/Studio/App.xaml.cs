a96d1541d6dd8d8db8ed6801d392e2832a85685d
Amer Koleci <amerkoleci@gmail.com>
2020-02-28 16:21:44 +0100

    New WIP WPF editor.

---
+++ src/Tools/Editor/Studio/App.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace Alimer.Studio
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
